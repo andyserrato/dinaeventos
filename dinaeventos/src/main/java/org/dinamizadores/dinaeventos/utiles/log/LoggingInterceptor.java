4ca12193b4ce569ad2b2d8e41afd185c9ea834de
Andy <andyserrato@hotmail.com>
2016-08-12 13:21:07 +0200

    Se cambia nivel de log del interceptor.
    se agrega la template a resources

--- dinaeventos/src/main/java/org/dinamizadores/dinaeventos/utiles/log/LoggingInterceptor.java
+++ dinaeventos/src/main/java/org/dinamizadores/dinaeventos/utiles/log/LoggingInterceptor.java
@@ -1,6 +1,5 @@
 package org.dinamizadores.dinaeventos.utiles.log;
 
-
 import java.io.Serializable;
 
 import javax.inject.Inject;
@@ -13,18 +12,19 @@ import org.apache.logging.log4j.Logger;
 @Interceptor
 @Loggable
 public class LoggingInterceptor implements Serializable {
-	
+
 	private static final long serialVersionUID = 742813988565528026L;
+
 	@Inject
 	private Logger logger;
-	
+
 	@AroundInvoke
 	public Object logMethod(InvocationContext ic) throws Exception {
-		logger.info(ic.getTarget().toString() + " Inicio " + ic.getMethod().getName());
+		logger.debug(ic.getTarget().toString() + " Inicio " + ic.getMethod().getName());
 		try {
 			return ic.proceed();
 		} finally {
-			logger.info(ic.getTarget().toString() + " Fin " + ic.getMethod().getName());
+			logger.debug(ic.getTarget().toString() + " Fin " + ic.getMethod().getName());
 		}
 	}
 }
